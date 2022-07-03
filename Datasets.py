0e7ad16198795fee887690445fb3621bbd0bf17f
Narvik86 <hxk151830@utdallas.edu>
2022-07-02 19:58:24 -0500

    main

--- Datasets.py
+++ Datasets.py
@@ -2,10 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandasql import sqldf
 
+
+pysqldf = lambda q: sqldf(q, globals())
 
 sns.set_theme(style='darkgrid')
 
 df = pd.read_csv('train.csv')
 
-print(df)
+#print(df)
+
+dfs = df[1:5]
+
+print(dfs)
+
+q = """SELECT Age 
+       FROM df 
+       LIMIT 10;"""
+
+names = pysqldf(q)
+print(names)
