25bb6457d8e5c2f6415365c418a00563b5246135
Narvik86 <hxk151830@utdallas.edu>
2022-07-05 13:41:35 -0500

    main

--- workingserver.py
+++ workingserver.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 # Users Table - User ID, Account Type, User Name, Password Hash, email, Date Created, & Last Login
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(16), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     #image_file = db.Column(db.String(20), nullable=False, default= 'default.jpg')
     password = db.Column(db.String(60), nullable=False)
